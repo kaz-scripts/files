6603cc037e4a0da349f9ecf277ec5457f3d790a9
kaz-scripts <77143406+kaz-scripts@users.noreply.github.com>
2022-12-05 20:14:43 +0900

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,2 +1,4 @@
+a=5000;
+b=7500;
 function sleep(waitMsec) {var startMsec = new Date();while (new Date() - startMsec < waitMsec);}sleep(a);document.querySelector('html').animate([{ transform: 'rotate(0deg)' },{ transform: 'rotate(360deg)' }],{duration: b,easing: 'linear',iterations: Infinity});
 //<script src="https://raw.githubusercontent.com/kaz-scripts/files/main/rotate.js"></script>
