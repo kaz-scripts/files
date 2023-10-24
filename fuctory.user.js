223901ce25d12d8498b21b4e6c0c2939d3e9380a
kaz-scripts <77143406+kaz-scripts@users.noreply.github.com>
2023-10-24 22:04:53 +0900

    Add files via upload

---
+++ fuctory.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         factory mouse
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  toggle with f
+// @author       wakka
+// @match        https://diep.io/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('t u=5.v.8,b=w.z.c;A 8,B=K(j(){L(!M.N("d-O").C.P[0].C.Q("R-S"))6},T);U(B);A 4={x:0,y:0,g:!1,9:!1},h=!1;5.f("V",e=>{4.x=e.W-5.k/2,4.y=e.X-5.l/2}),5.f("Y",e=>{0==e.i?4.g=!0:2==e.i&&(4.9=!0)}),5.f("Z",e=>{0==e.i?4.g=!1:2==e.i&&(4.9=!1)}),5.f("10",e=>{"D"===e.m?4.9=!0:"11"===e.m&&(h=!h)}),5.f("12",e=>{"D"===e.m&&(4.9=!1)});13 E 14 15{16(){17()}n F(){6"18"}o F(e){p q("r")}n G(){6[4.x/(5.k/3),4.y/(5.l/3),4.x/5.k,4.y/5.l]}o G(e){p q("r")}n H(){6[{7:!1},{7:!1},{7:!1},{7:!1},{7:!1},{7:!1},{7:4.9},{7:4.g}]}o H(e){p q("r")}}8=j(a){6 h?[19 E]:[]};t c=j(){1a(I){J 8:6 b.s(u);J c:6 b.s(b)}6 b.s(I)};w.z.c=c,5.v.8=8;',62,73,'||||mouse|window|return|pressed|getGamepads|shift||_toString|toString|||addEventListener|down|active|button|function|innerWidth|innerHeight|code|get|set|throw|RangeError|No|call|const|_getGamepads|navigator|Function|||prototype|let|load|shadowRoot|ShiftLeft|ArtificialGamepad|mapping|axes|buttons|this|case|setInterval|if|document|querySelector|base|children|getElementById|username|input|100|clearInterval|mousemove|clientX|clientY|mousedown|mouseup|keydown|KeyF|keyup|class|extends|EventTarget|constructor|super|standard|new|switch'.split('|'),0,{}))})();
